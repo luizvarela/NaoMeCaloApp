deb8f395f1c820625fea5caa53b5d9e620d4faf2
Luiz Varela <luiz.lvarela@gmail.com>
2015-04-22 01:43:05 -0300

    Configured bootstrap to application

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module NaoMeCaloApp
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
-
-    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
+  
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
   end
 end
