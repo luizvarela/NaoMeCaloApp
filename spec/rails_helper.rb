04abb16671d87faa73af351868159433d71082f2
Luiz Varela <luiz.lvarela@gmail.com>
2015-04-23 20:47:20 -0300

    Created support and lib folders inside spec folder and added to application.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'shoulda/matchers'
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
