bb655b30457e84844e4ae6b7a4d7ebcbf11d0b8b
Luiz Varela <luiz.lvarela@gmail.com>
2015-04-23 20:51:07 -0300

    List delatations on google maps inside index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @delatations = Delatation.all
+    @delatations_markers = Maps::DelatationMarker.new(@delatations).build_markers
   end
 end
