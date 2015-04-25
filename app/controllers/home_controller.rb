ee9a1cb6daf7c714b3e2bde3c51ca6a3e9a6c529
Luiz Varela <luiz.lvarela@gmail.com>
2015-04-25 15:10:10 -0300

    Removed home controller and moved index action to delatations controller

--- app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @delatation = Delatation.new
-    @delatations = Delatation.all
-    @delatations_markers = Maps::DelatationMarker.new(@delatations).build_markers
-  end
-end
