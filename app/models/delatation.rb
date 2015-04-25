134332f48caca24d705c6f00dda5cd5f06278cf0
Luiz Varela <luiz.lvarela@gmail.com>
2015-04-25 15:07:57 -0300

    Created enuns to category and shift field

--- app/models/delatation.rb
+++ app/models/delatation.rb
@@ -1,4 +1,12 @@
 class Delatation < ActiveRecord::Base
+  extend Enumerize
+
+  geocoded_by :address
+  after_validation :geocode 
+  
   validates_presence_of :category, :address, :occurrence_date,
-    :shift, :description, :longitude, :latitude
+    :shift, :description
+
+  enumerize :category, in: { business: 1, event: 2, street: 3 }
+  enumerize :shift, in: { morning: 1, afternoon: 2, night: 3, evening: 4 }
 end
